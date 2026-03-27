1ae00580eecb15fd4386e33b53052e380ef0648a
Sai Krishna <itss.saikrishnaz@gmail.com>
2026-03-27 11:52:31 +0530

    Change Redis connection settings
    Updated Redis connection details with new host and port.

--- Order_server_kafka.py
+++ Order_server_kafka.py
@@ -80,12 +80,12 @@ PRODUCER = KafkaProducer(
 # REDIS
 # =====================================================
 redis_conn_str = redis.Redis(
-                host='redis-19731.crce182.ap-south-1-1.ec2.cloud.redislabs.com',
-                port=19731,
-                decode_responses=True,
-                username="default",
-                password="xJOOwytWRYTeFZUCBXAg1CAdQDzdLWKG",
-            ) 
+    host='redis-11429.crce179.ap-south-1-1.ec2.cloud.redislabs.com',
+    port=11429,
+    decode_responses=True,
+    username="default",
+    password="IKomAGomiMlxqLJbZxsL1SAsv59ZBB3H",
+)
 
 
 def read_redis(key):
